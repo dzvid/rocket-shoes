9a870a1e381ee46e782294c3a2bd65a2d0df0e1f
tukno <oliveiradavid007@gmail.com>
2020-02-15 01:44:37 -0400

    chore: Dynamic fake server depending on development or prod environment

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,10 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:3333',
+  baseURL:
+    process.env.NODE_ENV === 'development'
+      ? 'http://localhost:3333'
+      : 'https://my-json-server.typicode.com/tukno/rocket-shoes',
 });
 
 export default api;
